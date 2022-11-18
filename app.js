c6e0b541ec434710bec1f547adea2013f389d4eb
Mahfuz Mansur <mansurmahfuz080@gmail.com>
2022-11-18 11:28:23 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+// // initai references
+// const newTaskinput = document.querySelector("#new-task input");
+// const taskDiv = document.querySelector("#tasks")
+// let deleteTasks, editTasks,tasks;
+// let updateTasks ="";
+// let count;
+
+// // function on window load
+
+// window.onload = () => {
+//     updateNote ="";
+//     count = Object.keys(localStorage).length;
+//     displayTask();
+
+// };
+
+
+// // function to Display The tasks
+// counst displayTask = () => {
+//     if (Object.keys(localStorage).length > 0){
+//         taskDiv.style.display = "inline-block";
+//     }  else}
+//     tasksDiv.style.display = "none";
+// {
+//     // clear the tasks
+//     tasksDiv.innerHTML ="";
+    
+//     // fetch All the keys in local Storage
+//     let tasks = Object.keys(localStorage);
+//     tasks = tasks.sort();
+
+// for (let key of tasks){
+//     let classValue = "";
+// }
+
+
+// // get all values
+
+// let value = localStorage.getItem(key);
+// let taskInnerDiv = document
